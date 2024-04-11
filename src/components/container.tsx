11cccb3cf3bd76f9864b0bd13b7b47cd2ce6267a
naveen.singh <naveen.singh@itelasoft.com>
2024-04-11 10:10:27 +0530

    minor change

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,7 +2,7 @@ type ContainerProps = {
   children: React.ReactNode;
 };
 
-export default function Container({ children }: ContainerProps) {
+export default function Container({ children }: Readonly<ContainerProps>) {
   return (
     <div className="max-w-[1100px] mx-auto bg-white min-h-screen flex flex-col border-l border-r">
       {children}
